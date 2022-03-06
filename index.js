28e249341e5bdd1427509353a957f203db477e81
Glitch (remarkable-marred-curtain) <none>
2022-03-06 18:01:41 +0000

    💎🎧 Updated with Glitch

---
+++ index.js
@@ -0,0 +1,124 @@
+const express = require('express');
+const { Client, RichEmbed } = require('discord.js');
+const app = express();
+    function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+const http = require('http');
+app.get("/", (request, response) => {
+  console.log(Date.now() + "  Token Sese Sokma | Hostlandı");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000) 
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const client1 = new Discord.Client();
+const client2 = new Discord.Client();
+const client3 = new Discord.Client();
+const client4 = new Discord.Client();
+const client5 = new Discord.Client();
+const client6 = new Discord.Client();
+const client7 = new Discord.Client();
+const client8 = new Discord.Client();
+const client9 = new Discord.Client();
+const client10 = new Discord.Client();
+const client11 = new Discord.Client();
+const client12 = new Discord.Client();
+const client13 = new Discord.Client();
+const client14 = new Discord.Client();
+const client15 = new Discord.Client();
+const client16 = new Discord.Client();
+const client17 = new Discord.Client();
+const client18 = new Discord.Client();
+const client19 = new Discord.Client();
+const client20 = new Discord.Client();
+const client21 = new Discord.Client();
+const client22 = new Discord.Client();
+const client23 = new Discord.Client();
+const client24 = new Discord.Client();
+const client25 = new Discord.Client();
+
+//Erdem Çakıroğlu Sunar Kolay Gelsin.
+
+const data = new Map();
+
+
+
+client.login(process.env.token)//Hesap Tokeni Token
+
+client.on('ready', ()=>{
+client.channels.get('948038938533167154').join()//ses kanal id
+})
+
+client1.login(process.env.token1)
+
+client1.on('ready', ()=>{
+client1.channels.get('948038938533167154').join()
+})
+
+
+client2.login(process.env.token2)
+
+client2.on('ready', ()=>{
+client2.channels.get('948038938533167154').join()
+})
+
+
+client3.login(process.env.token3)
+
+client3.on('ready', ()=>{
+client3.channels.get('948038938533167154').join()
+})
+
+
+
+client4.login(process.env.token4)
+
+client4.on('ready', ()=>{
+client4.channels.get('948038938533167154').join()
+})
+
+
+client5.login(process.env.token5)
+
+client5.on('ready', ()=>{
+client5.channels.get('948038938533167154').join()
+})
+
+
+client6.login(process.env.token6)
+
+client6.on('ready', ()=>{
+client6.channels.get('948038938533167154').join()
+})
+
+
+client7.login(process.env.token7)
+
+client7.on('ready', ()=>{
+client7.channels.get('948038938533167154').join()
+})
+
+
+client8.login(process.env.token7)
+
+client8.on('ready', ()=>{
+client8.channels.get('948038938533167154').join()
+})
+
+
+client9.login(process.env.token7)
+
+client9.on('ready', ()=>{
+client9.channels.get('948038938533167154').join()
+})
+
+//Erdem Çakıroğlu Sunar Kolay Gelsin.//Erdem Çakıroğlu Sunar Kolay Gelsin.//Erdem Çakıroğlu Sunar Kolay Gelsin.
